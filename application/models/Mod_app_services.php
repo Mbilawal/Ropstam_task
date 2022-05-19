fa515fd82261e5c63da0749b7e89ed2719e9b82a
Muhammad Bilawal <59437289+Mbilawal@users.noreply.github.com>
2022-05-19 09:15:12 +0500

    Update Mod_app_services.php

--- application/models/Mod_app_services.php
+++ application/models/Mod_app_services.php
@@ -18,9 +18,22 @@ class Mod_app_services extends CI_Model
 			$where_arr['$and'][] = array('id' => $product_id);
 
 			$document = $this->mongo_db->customQuery();
-        	$return = (array) $document->products->findOne($where_arr, array('projection' => array('_id' => 0,'name' => 1,'price' => 1,'size' => 1,'color' => 1,'images_arr' => 1), 'sort' => array('id'=> -1)));
+        	$return = (array) $document->products->findOne($where_arr, array('projection' => array('_id' => 0,'name' => 1,'price' => 1,'size' => 1,'color' => 1,'images_arr' => 1,'review_arr' => 1,'rating_arr' => 1), 'sort' => array('id'=> -1)));
 
 			if ($return) :
+
+				if($return['rating_arr']){
+					$rating_arr = (array) $return['rating_arr'];
+					$return['t_rating'] = count($rating_arr);
+					unset($return['rating_arr']);
+				}
+
+				if($return['review_arr']){
+					$review_arr = (array) $return['review_arr'];
+					$return['t_review'] = count($review_arr);
+					unset($return['review_arr']);
+				}				
+
 				return $return;
 			else :
 				return false;
